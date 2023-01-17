ce9a7d3bd05f35defb97fc65595f44958b9a54c7
nunegabriel <nune.gabriel.gn@gmail.com>
2023-01-17 19:37:52 +0000

    FIX LINTERS

--- student.rb
+++ student.rb
@@ -1,13 +1,13 @@
 require "./person.rb"
 
 class Student < Person
-    attr_accessor :classroom
+  attr_accessor :classroom
 
-    def initialize(name, age, parent_permission)
-    end
+  def initialize(name, age, parent_permission, classroom)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        return "¯\(ツ)/¯"
-    end
-
+  def play_hooky
+    return "¯\(ツ)/¯"
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,13 @@
 require "./person.rb"
 
 class Teacher < Person
-    attr_accessor : specialization
+  attr_accessor :specialization
 
-    def initialize(specialization)
-    @specialization = specialization 
-    end
+  def initialize(specialization, name, age, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services
-        true
-    end
-
+  def can_use_services
+    true
+  end
 end
