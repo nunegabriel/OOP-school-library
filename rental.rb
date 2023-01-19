95292575b55915920b6f06ca6bbbdb3a65238648
nunegabriel <nune.gabriel.gn@gmail.com>
2023-01-19 16:46:44 +0000

    UPDATE RENTALL

--- rental.rb
+++ rental.rb
@@ -1,13 +1,11 @@
 class Rental
 attr_accessor :date :person :book
 
-def initialize(date,person,book)
+def initialize(date)
     @date = date
     @person = person
     person.rentals << self
-
     @book = book
     book.rentals << self
-
 end
 end
