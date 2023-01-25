9487b3a0b581321b5131cc27ac76561f9569ce75
Bludidi <bludidi5@gmail.com>
2023-01-25 13:46:27 +0200

    fix linters

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
 require './app'
 
 class Menu
-def main
-  books = []
-  rental_data = []
-  people = []
-  funct(books, people, rental_data)
-end
+  def main
+    books = []
+    rental_data = []
+    people = []
+    funct(books, people, rental_data)
+  end
 end
 
 main = Menu.new
