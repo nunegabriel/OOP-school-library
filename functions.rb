2d5bfa4db47144638b419387e2284f1c73229947
Bludidi <bludidi5@gmail.com>
2023-01-26 17:56:46 +0200

    add spaces

--- functions.rb
+++ functions.rb
@@ -1,3 +1,5 @@
+# require './person'
+
 class Function
   def initialize
     @books = []
@@ -11,13 +13,13 @@ def show_books(books)
     puts 'No books found in library'
   else
     books.each do |book|
-      puts "title: #{book.title}, Author: #{book.author}"
+      puts 'title: #{book.title}, Author: #{book.author}'
     end
   end
 end
 
 def add_people(people)
-  puts '1.Add a student or 2.Add a teacher'
+  puts '1. Add a student or 2. Add a teacher'
   person_to_add = gets.chomp.to_i
   print 'Age? '
   age = gets.chomp.to_i
@@ -26,13 +28,13 @@ def add_people(people)
 
   case person_to_add
   when 1
-    print 'Do you have parent Permission? [Y/N]:'
+    print 'Do you have parent Permission? [Y/N]: '
     user_response = gets.chomp.capitalize
     user_permission = true if user_response == 'Y'
     user_permission = false if user_response == 'N'
     people << Student.new(nil, age, name, parent_permission: user_permission)
   when 2
-    print 'Specialisation?'
+    print 'Specialisation? '
     specialisation = gets.chomp
     people << Teacher.new(specialisation, age, name)
   end
@@ -41,9 +43,9 @@ end
 
 def add_book(books)
   puts 'Please Add a book'
-  print 'Title :'
+  print 'Title: '
   title = gets.chomp.capitalize
-  print 'Author:'
+  print 'Author: '
   author = gets.chomp.capitalize
   books << Book.new(title, author)
   puts 'Book added successfully'
@@ -70,7 +72,7 @@ def add_rental(books, people, rental_data)
 end
 
 def show_rental_data(rentals)
-  print 'Enter Person ID'
+  print 'Enter Person ID: '
   selected_id = gets.chomp.to_i
   rentals.each do |rental|
     next unless rental.person.id == selected_id
