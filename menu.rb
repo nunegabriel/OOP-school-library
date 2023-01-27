ca02fc80873e64dbba4f7d161a936e4236898b99
Bludidi <bludidi5@gmail.com>
2023-01-27 08:32:07 +0200

    update app.rb file

---
+++ menu.rb
@@ -0,0 +1,13 @@
+require './app'
+
+def display_menu
+  puts 'Welcome to the school library'
+  puts ' Please choose an option by entering a number: '
+  puts '1: list all books.'
+  puts '2: list all people.'
+  puts '3: Create a person'
+  puts '4: Create a book'
+  puts '5: Create a rental'
+  puts '6: List all rented books to a person by ID'
+  puts '7: Exit'
+end
