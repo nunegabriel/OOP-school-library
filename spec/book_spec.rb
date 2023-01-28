e01f5162d10e9b26e6e217615067d1278ccea134
nunegabriel <nune.gabriel.gn@gmail.com>
2023-01-28 09:03:48 +0000

    ADD COOOK TEST

---
+++ spec/book_spec.rb
@@ -0,0 +1,17 @@
+require 'book'
+
+describe Book do
+  context 'When testing the Book class' do
+    book = Book.new('Gabz', 'John')
+
+    it 'expects to display Book title and Book author' do
+      expect(book).to be_an_instance_of Book
+    end
+    it 'title of book' do
+      expect(book.title).to eq 'Ruby'
+    end
+    it 'author of book' do
+      expect(book.author).to eq 'Jose'
+    end
+  end
+end
